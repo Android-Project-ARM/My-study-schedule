3ad839b58340521997aaff23c3038ad3c0a30ec5
MarchyV <barros.marcel92@gmail.com>
2015-04-10 21:29:32 +0200

    (feat) basic class implementation

--- MyApplication2/app/src/main/java/com/example/marcelba/mystudyschedule/Inicio.java
+++ MyApplication2/app/src/main/java/com/example/marcelba/mystudyschedule/Inicio.java
@@ -1,17 +1,31 @@
 package com.example.marcelba.mystudyschedule;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class Inicio extends ActionBarActivity {
+public class Inicio extends ActionBarActivity implements Button.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+        Button horaryButton = ((Button)this.findViewById(R.id.horaryButton));
+        Button subjectsButton = ((Button)this.findViewById(R.id.subjectsButton));
+        Button tasksButton = ((Button)this.findViewById(R.id.tasksButton));
+        Button evaluationButton = ((Button)this.findViewById(R.id.evaluationButton));
+        horaryButton.setOnClickListener(this);
+        subjectsButton.setOnClickListener(this);
+        tasksButton.setOnClickListener(this);
+        evaluationButton.setOnClickListener(this);
+        // MenuItem item= (MenuItem) findViewById(R.id.action_settings);
+        //item.setVisible(false);
+
     }
 
 
@@ -31,9 +45,36 @@ public class Inicio extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, Ajustes.class);
+            startActivity(intent);
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onClick(View v) {
+       Intent intent=null;
+        switch(v.getId()){
+            case R.id.horaryButton:
+                intent = new Intent(this, Horario.class);
+                break;
+            case R.id.subjectsButton:
+                intent = new Intent(this, Asignaturas.class);
+                break;
+            case R.id.tasksButton:
+                intent = new Intent(this, Tareas.class);
+                break;
+            case R.id.evaluationButton:
+                intent = new Intent(this, Evaluacion.class);
+                break;
+              default:
+                intent = new Intent(this, Inicio.class);
+                break;
+            }
+        startActivity(intent);
+
+         }
+
 }
