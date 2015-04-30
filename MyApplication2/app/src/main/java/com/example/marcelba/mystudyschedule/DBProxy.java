b5aa8b9e325042aa8d248c3831a72aa7db04d49e
Rubén Ruiz Herrera <rubnruizherrera@gmail.com>
2015-05-01 01:11:05 +0200

    añadir a la BBDD
    añade datos a la BBDD falta testear

--- MyApplication2/app/src/main/java/com/example/marcelba/mystudyschedule/DBProxy.java
+++ MyApplication2/app/src/main/java/com/example/marcelba/mystudyschedule/DBProxy.java
@@ -2,6 +2,7 @@ package com.example.marcelba.mystudyschedule;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
@@ -29,6 +30,18 @@ public class DBProxy extends SQLiteOpenHelper {
     final public static String DB_HORARY_COL_STARTTIME = "startime";
     final public static String DB_HORARY_COL_ENDTIME = "endtime";
 
+
+    final public static String DB_TASK_TABLE_NAME = "ToDo";
+    final public static String DB_TASK_COL_ID = BaseColumns._ID;
+    final public static String DB_TASK_COL_NAME = "TareaNombre";
+    final public static String DB_TASK_COL_SUBJECT_ID = "TareaAsignaturaID";
+    final public static String DB_TASK_COL_ENDDATE = "TareaFechaEntrega";
+    final public static String DB_TASK_COL_PRIORITY = "TareaPrioridad";
+    final public static String DB_TASK_COL_DESC = "TareaDescripcion";
+
+
+
+
     public DBProxy(Context context){
         super(context, DB_NAME, null, DB_VERSION);
 
@@ -38,6 +51,22 @@ public class DBProxy extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE "+DB_SUBJECTS_TABLE_NAME+"("+DB_SUBJECTS_COL_ID+" INTEGER PRIMARY KEY,"+DB_SUBJECTS_COL_NAME+" TEXT,"+DB_SUBJECTS_COL_COLOR+" TEXT,"+DB_SUBJECTS_COL_TEACHER+"TEXT)" );
         db.execSQL("CREATE TABLE "+DB_HORARY_TABLE_NAME+"("+DB_HORARY_COL_ID+" INTEGER PRIMARY KEY,"+DB_HORARY_COL_SUBJECT_ID+" INTEGER,"+DB_HORARY_COL_WEEKDAY+" INTEGER,"+DB_HORARY_COL_STARTTIME+" TEXT,"+DB_HORARY_COL_ENDTIME+"TEXT)" );
+
+
+
+        db.execSQL("CREATE TABLE "+DB_TASK_TABLE_NAME+"("+
+                        DB_TASK_COL_ID+" INTEGER PRIMARY KEY,"+
+                        DB_TASK_COL_NAME+" TEXT,"+
+                        DB_TASK_COL_SUBJECT_ID+" INTEGER,"+
+                        DB_TASK_COL_ENDDATE+" DATE,"+
+                        DB_TASK_COL_PRIORITY+" FLOAT,"+
+                        DB_TASK_COL_DESC+"TEXT)" );
+
+
+
+
+
+
     }
 
     @Override
@@ -57,42 +86,26 @@ public class DBProxy extends SQLiteOpenHelper {
 
     }
 
-   /* public Cursor ReadNotes() {
 
+
+
+    public Cursor ReadTask() {
         SQLiteDatabase db = getReadableDatabase();
-        String[] columns = {DB_COL_ID, DB_COL_TITLE, DB_COL_NOTE};
-        return db.query(DB_TABLE_NAME, columns, null, null, null, null, null);
+        String[] columns = {DB_TASK_COL_ID, DB_TASK_COL_NAME, DB_TASK_COL_SUBJECT_ID, DB_TASK_COL_ENDDATE, DB_TASK_COL_PRIORITY,  DB_TASK_COL_DESC};
+        return db.query(DB_TASK_TABLE_NAME, columns, null, null, null, null, null);
 
     }
 
-    public void updateNote(long id, String title, String body)
-    {
+    public void AddTask(String addNuevaTarea, String addFechaTarea, Float addRatTarea, String addDescTarea) {
         SQLiteDatabase db = getWritableDatabase();
-
-// New value for one column
         ContentValues values = new ContentValues();
-        values.put(DB_COL_TITLE, title);
-        values.put(DB_COL_NOTE, body);
+        values.putNull(DB_TASK_COL_ID);
+        values.put(DB_TASK_COL_NAME, addNuevaTarea);
+        values.put(DB_TASK_COL_ENDDATE, addFechaTarea);
+        values.put(DB_TASK_COL_PRIORITY, addRatTarea);
+        values.put(DB_TASK_COL_DESC, addDescTarea);
 
-// Which row to update, based on the ID
-        String selection = DB_COL_ID + " LIKE ?";
-        String[] selectionArgs = { String.valueOf(id) };
+        db.insert(DB_TASK_TABLE_NAME, null, values);
 
-        int count = db.update(
-                DB_TABLE_NAME,
-                values,
-                selection,
-                selectionArgs);
     }
-
-    public void DelNote(long id)
-    {
-        SQLiteDatabase db = getWritableDatabase();
-        String table = DB_TABLE_NAME;
-        String whereClause = "_id" + "=?";
-        String[] whereArgs = new String[] { String.valueOf(id) };
-        db.delete(table, whereClause, whereArgs);
-
-
-    }*/
 }
---
+++ MyApplication2/app/src/main/java/com/example/marcelba/mystudyschedule/NewTask.java
@@ -0,0 +1,87 @@
+package com.example.marcelba.mystudyschedule;
+
+import android.media.Rating;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Spinner;
+
+/**
+ * Created by iKaoos on 30/4/15.
+ */
+
+
+
+public class NewTask  extends ActionBarActivity {
+
+    public Integer idTarea;
+    public boolean update = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nueva_tarea);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_nueva_tarea, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+
+
+    protected void onPause() {
+        super.onPause();
+
+
+
+
+            EditText NuevaTarea = (EditText) findViewById(R.id.EtNuevaTarea);
+            String addNuevaTarea = NuevaTarea.getText().toString();
+
+           /** Spinner AsignaturaTarea = (Spinner) findViewById(R.id.SnAsignatura);
+            String addAsigTarea = AsignaturaTarea **/
+
+            EditText FechaTarea = (EditText)findViewById(R.id.EtFechaTarea);
+            String addFechaTarea = FechaTarea.getText().toString();
+
+            RatingBar RatTarea = (RatingBar) findViewById(R.id.RatingTarea);
+            Float addRatTarea = RatTarea.getRating();
+
+            EditText DescTarea = (EditText)findViewById(R.id.EtDescTarea);
+            String addDescTarea = DescTarea.getText().toString();
+
+
+
+            Tasks.db.AddTask(addNuevaTarea, addFechaTarea, addRatTarea, addDescTarea );
+
+
+    }
+
+
+
+
+}
